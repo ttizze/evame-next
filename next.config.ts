cb6be221d54f27ce74474ccd68a24b581975f6d1
ttizze <tomoki2757@gmail.com>
2024-12-20 13:04:07 +0900

    f

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+ import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+  const nextConfig: NextConfig = {
+    /* config options here */
+  };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
